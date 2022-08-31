9328d3b5ff29f8bf0fd51d7b4045f7a3ebf975bd
John Chiu <john@makertec.net>
2022-08-31 17:59:30 +0800

    feat: layout

---
+++ components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card">
+    <div class="card-image relative">
+      <button class="card-button">
+        <font-awesome-icon icon="fa-solid fa-link" style="color: #566675" />
+      </button>
+      <img src="https://via.placeholder.com/150/92c952" alt="" />
+    </div>
+    <div class="card-body">accusamus rewq</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'NuxtTutorial',
+  setup() {
+    const message = ref('this is card')
+
+    return {
+      message,
+    }
+  },
+  head: {},
+})
+</script>
+
+<style lang="scss">
+.card {
+  background-color: #dfe6ed;
+  &-button {
+    position: absolute;
+    top: 4px;
+    right: 4px;
+    padding: 4px;
+    background-color: #c3cfd9;
+    border-radius: 50%;
+    transition: all 300ms;
+
+    &:hover {
+      background-color: #7f878d;
+    }
+  }
+  &-body {
+    background-color: white;
+    margin: 0 2px 2px 2px;
+    padding: 8px 4px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <Tutorial />
+  <div class="index">
+    <div class="container mx-auto">
+      <h1 class="text-2xl font-bold text-center py-8">Aotter Gallery</h1>
+      <section class="card-list">
+        <Card v-for="(item, index) in cardList" :key="index" />
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
   name: 'IndexPage',
-}
+  setup() {
+    const message = ref('this is card')
+    const cardList = ref([...Array(60)].map((v) => v))
+
+    return {
+      message,
+      cardList,
+    }
+  },
+})
 </script>
+
+<style lang="scss">
+.card-list {
+  @apply mb-8;
+  @apply flex flex-wrap justify-around;
+  @apply gap-5 md:gap-10 lg:gap-20;
+}
+</style>
