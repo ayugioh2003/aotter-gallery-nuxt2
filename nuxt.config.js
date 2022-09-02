4540c0a3970fc1322f6a890e0d24df99058a8b75
John Chiu <john@makertec.net>
2022-09-02 18:00:53 +0800

    feat: facebook smo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'aotter-gallery-nuxt2',
+    title: 'Aotter Gallery',
     htmlAttrs: {
       lang: 'en',
     },
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="index">
     <div class="mx-auto" style="max-width: 1000px; padding: 0, 32px">
       <h1 class="text-2xl font-bold text-center py-24">Aotter Gallery</h1>
-      <section v-if="photos.length > 0" class="card-list">
+      <section v-if="photos && photos.length > 0" class="card-list">
         <Card
           v-for="(item, index) in photos.filter((v, i) => i < 20)"
           :id="item.id"
@@ -17,21 +17,55 @@
 </template>
 
 <script>
-import { defineComponent, useAsync } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  ref,
+  useFetch,
+  useRoute,
+  computed,
+  useMeta,
+} from '@nuxtjs/composition-api'
 import { holderApi } from '@/api/holder'
 
 export default defineComponent({
   name: 'IndexPage',
   setup() {
     const { fetchPhotos } = holderApi()
+    const { meta } = useMeta()
+    const route = useRoute()
 
-    const photos = useAsync(() => fetchPhotos())
+    const photos = ref()
+
+    // 取得 photos 後要做的事情
+    useFetch(async () => {
+      // 1. 取得 photos
+      photos.value = await fetchPhotos()
+
+      // 2. 分享 FB 時，根據圖片 id 抓取對應資訊
+      const id = computed(() => route.value.query.id)
+      const photo = photos.value.find((photo) => {
+        return Number(id.value) === Number(photo.id)
+      })
+
+      if (id && photo) {
+        meta.value.push({
+          hid: 'ogDescription',
+          name: 'og:description',
+          content: photo.title,
+        })
+        meta.value.push({
+          hid: 'ogImage',
+          name: 'og:image',
+          content: photo.url,
+        })
+      }
+    })
 
     return {
       photos,
-      // loadPhotos,
     }
   },
+  head: {},
 })
 </script>
 
